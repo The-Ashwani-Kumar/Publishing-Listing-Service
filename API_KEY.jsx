a3721fa22a8924c4de1212fd71f53d2cdd1331d7
Ashwani Kumar <ashwanikumar26102001@gmail.com>
2025-02-15 12:06:13 +0530

    Encrypted API_KEY.jsx

--- API_KEY.jsx
+++ API_KEY.jsx
@@ -1,3 +1,3 @@
-const REACT_APP_API_KEY = "AIzaSyBOk6BlaFmdthYm9o3kqRHfZKRrWGr_18M";
+const REACT_APP_API_KEY = "AIzaSyBOk****************rWGr_18M";
 console.log(import.meta.env.VITE_MY_USER_ID);
 export default REACT_APP_API_KEY
